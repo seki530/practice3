d7ba0deea2d70200b7f3f068c73bfcd373a1678c
Seki Ryota <sekiryouta749@gmail.com>
2023-12-26 23:59:59 +0900

    モデル、コントローラー作成

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,10 @@
+class TodosController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
---
+++ db/migrate/20240330071056_create_todos.rb
@@ -0,0 +1,9 @@
+class CreateTodos < ActiveRecord::Migration[6.1]
+  def change
+    create_table :todos do |t|
+      t.text :title
+
+      t.timestamps
+    end
+  end
+end
