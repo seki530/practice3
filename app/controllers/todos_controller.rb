846815ab5cf0f8a965b006824dfd280eb4dd9891
Seki Ryota <sekiryouta749@gmail.com>
2024-01-06 23:59:59 +0900

    新規タスクの作成

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -3,8 +3,21 @@ class TodosController < ApplicationController
   end
 
   def new
+    @todo = Todo.new
+  end
+  
+  def create
+    @todo = Todo.create(todo_params)
+    @todo.save
+    redirect_to todos_path
   end
 
   def edit
   end
+  
+  private
+    def todo_params
+      params.require(:todo).permit(:title)
+    end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'todos/index'
-  get 'todos/new'
-  get 'todos/edit'
+  resources :todos
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
